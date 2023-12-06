19aff3a50f103b1e1ecbaf275e9cc8a4f97a8171
Ansh Riyal <ansh.riyal@gmail.com>
2023-12-06 04:35:22 -0500

    Compiled 1 models.py and created combined pipeline for training and evaluation on hidden dataset + saving submission tensor

---
+++ combined_pipeline.py
@@ -0,0 +1,154 @@
+# Need to separate all files into function defintions and main.py part
+from models import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+# Data Loader.
+class CustomDataset(Dataset):
+    def __init__(self, num_of_vids=1000, evaluation_mode=False):
+        self.evaluation_mode = evaluation_mode
+        if self.evaluation_mode:
+            self.mode = 'hidden'
+            start_num = 15000
+        else:
+            self.mode = 'train'
+            start_num = 0
+        self.vid_indexes = torch.tensor([i for i in range(start_num, num_of_vids + start_num)])
+        self.num_of_vids = num_of_vids
+        
+
+    def __len__(self):
+        return self.num_of_vids
+    
+    def __getitem__(self, idx):
+        num_hidden_frames = 11
+        num_total_frames = 22
+        x = []
+        i = self.vid_indexes[idx]
+        
+        base_dir = './../Dataset_Student/'
+        
+        filepath = f'{base_dir}{self.mode}/video_{i}/'
+        # obtain x values.
+        for j in range(num_hidden_frames):
+            x.append(torch.tensor(plt.imread(filepath + f'image_{j}.png')).permute(2, 0, 1))
+        x = torch.stack(x, 0)
+        
+        if self.evaluation_mode:
+            return x
+        
+        file_path = f"{base_dir}{self.mode}/video_{i}/mask.npy"
+        y = np.load(file_path)[21]  # last frame.
+        return x, y
+    
+    
+# Dataloader
+batch_size = 16
+
+# Create Train DataLoader
+num_videos = 1000
+train_data = CustomDataset(num_videos)
+# load the data.
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
+# Create Val DataLoader
+num_val_videos = 1000
+val_data = CustomDataset(num_val_videos, evaluation_mode = True)
+# load the data.
+val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
+
+gpu_name = 'cuda'
+device = torch.device(gpu_name if torch.cuda.is_available() else 'cpu')
+
+class combined_model(nn.Module):
+    def __init__(self, device):
+        super(combined_model, self).__init__()
+        self.frame_prediction_model = DLModelVideoPrediction((11, 3, 160, 240), 64, 512, groups=4)
+        self.frame_prediction_model = nn.DataParallel(self.frame_prediction_model)
+        self.frame_prediction_model = self.frame_prediction_model.to(device)
+
+        self.image_segmentation_model = unet_model()
+        self.image_segmentation_model = nn.DataParallel(self.image_segmentation_model)
+        self.image_segmentation_model = self.image_segmentation_model.to(device)
+        
+    def load_weights(self):
+        best_model_path = './checkpoints/frame_prediction.pth'  # load saved model to restart from previous best model
+        if os.path.isfile(best_model_path):
+            print('frame prediction model weights found')
+            self.frame_prediction_model.load_state_dict(torch.load(best_model_path))
+
+        best_model_path = './checkpoints/image_segmentation.pth'  # load saved model to restart from previous best model
+        if os.path.isfile(best_model_path):
+            print('image segmentation model weights found')
+            self.image_segmentation_model.load_state_dict(torch.load(best_model_path))
+            
+    def save_weights(self):
+        torch.save(self.frame_prediction_model.state_dict(), './checkpoints/frame_prediction.pth')
+        torch.save(self.frame_prediction_model.state_dict(), './checkpoints/image_segmentation.pth')
+        print('model weights saved successfully')
+        
+        
+    def forward(self,x):
+        x = self.frame_prediction_model(x)
+#         print(x.shape)
+        x = x[:,-1]
+#         print(x.shape)
+        x = self.image_segmentation_model(x)
+#         print(x.shape)
+        return x
+
+# Hyperparameters:
+num_epochs = 10
+lr = 0.0001
+model = combined_model(device)
+model.load_weights()
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr)
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.001, steps_per_epoch=len(train_loader),
+                                                epochs=num_epochs)
+
+train_losses = []
+preds_per_epoch = []
+for epoch in range(num_epochs):
+    train_loss = []
+    model.train()
+    train_pbar = tqdm(train_loader)
+
+    for batch_x, batch_y in train_pbar:
+        optimizer.zero_grad()
+        batch_x, batch_y = batch_x.to(device), batch_y.to(device).long()
+        pred_y = model(batch_x)#.long()
+        loss = criterion(pred_y, batch_y)
+        train_loss.append(loss.item())
+        train_pbar.set_description('train loss: {:.4f}'.format(loss.item()))
+#         print(loss)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+    train_loss = np.average(train_loss)
+    print(f"Average train loss {train_loss}")
+    train_losses.append(train_loss)
+#     torch.save(model.state_dict(), './checkpoint_frame_prediction.pth')
+    model.save_weights()
+    val_loss = []
+    model.eval()
+    val_pbar = tqdm(val_loader)
+
+    with torch.no_grad():
+        if epoch % 2 == 0:
+            for batch_x in val_pbar:
+                batch_x = batch_x.to(device)
+                pred_y = model(batch_x).float()
+                preds_per_epoch.append(pred_y)
+                
+latest_predictions = preds_per_epoch[-1]
+torch.save(latest_predictions, 'The_Big_Epochalypse_submission.pt')
---
+++ models.py
@@ -0,0 +1,277 @@
+# This file will have the class definitions for all of our model classes
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchmetrics.image import StructuralSimilarityIndexMeasure as SSIM
+
+import sys
+
+
+# Segmentation Mask - Unet model:
+
+class encoding_block(nn.Module):
+    
+    def __init__(self, in_channels, out_channels):
+        super(encoding_block, self).__init__()
+        model = []
+        model.append(nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False))
+        model.append(nn.BatchNorm2d(out_channels))
+        model.append(nn.ReLU(inplace=True))
+        model.append(nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False))
+        model.append(nn.BatchNorm2d(out_channels))
+        model.append(nn.ReLU(inplace=True))
+        self.conv = nn.Sequential(*model)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class unet_model(nn.Module):
+    '''
+    example usage:
+    model = unet_model()
+    model = nn.DataParallel(model)
+    model = model.to(device)
+    '''
+    def __init__(self, out_channels=49, features=[64, 128, 256, 512]):
+        super(unet_model, self).__init__()
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv1 = encoding_block(3, features[0])
+        self.conv2 = encoding_block(features[0], features[1])
+        self.conv3 = encoding_block(features[1], features[2])
+        self.conv4 = encoding_block(features[2], features[3])
+        self.conv5 = encoding_block(features[3] * 2, features[3])
+        self.conv6 = encoding_block(features[3], features[2])
+        self.conv7 = encoding_block(features[2], features[1])
+        self.conv8 = encoding_block(features[1], features[0])
+        self.tconv1 = nn.ConvTranspose2d(features[-1] * 2, features[-1], kernel_size=2, stride=2)
+        self.tconv2 = nn.ConvTranspose2d(features[-1], features[-2], kernel_size=2, stride=2)
+        self.tconv3 = nn.ConvTranspose2d(features[-2], features[-3], kernel_size=2, stride=2)
+        self.tconv4 = nn.ConvTranspose2d(features[-3], features[-4], kernel_size=2, stride=2)
+        self.bottleneck = encoding_block(features[3], features[3] * 2)
+        self.final_layer = nn.Conv2d(features[0], out_channels, kernel_size=1)
+
+    def forward(self, x):
+        skip_connections = []
+        x = self.conv1(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv3(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.conv4(x)
+        skip_connections.append(x)
+        x = self.pool(x)
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+        x = self.tconv1(x)
+        x = torch.cat((skip_connections[0], x), dim=1)
+        x = self.conv5(x)
+        x = self.tconv2(x)
+        x = torch.cat((skip_connections[1], x), dim=1)
+        x = self.conv6(x)
+        x = self.tconv3(x)
+        x = torch.cat((skip_connections[2], x), dim=1)
+        x = self.conv7(x)
+        x = self.tconv4(x)
+        x = torch.cat((skip_connections[3], x), dim=1)
+        x = self.conv8(x)
+        x = self.final_layer(x)
+        return x
+
+# Frame Prediction model:
+
+class CNN_Encoder(nn.Module):
+    def __init__(self, input_channels, hidden_channels):
+        super().__init__()
+        k_size = 3
+        p_len = 1
+        self.input_encoder = nn.Sequential(
+            nn.Conv2d(input_channels, hidden_channels, kernel_size=k_size, stride=1, padding=p_len),
+            nn.GroupNorm(2, hidden_channels),
+            nn.LeakyReLU(0.2))
+        self.encoder = nn.Sequential(
+            nn.Conv2d(hidden_channels, hidden_channels, kernel_size=k_size, stride=2, padding=p_len),
+            nn.GroupNorm(2, hidden_channels),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(hidden_channels, hidden_channels, kernel_size=k_size, stride=1, padding=p_len),
+            nn.GroupNorm(2, hidden_channels),
+            nn.LeakyReLU(0.2),
+            nn.Conv2d(hidden_channels, hidden_channels, kernel_size=k_size, stride=2, padding=p_len),
+            nn.GroupNorm(2, hidden_channels),
+            nn.LeakyReLU(0.2))
+
+    def forward(self, x):
+        x = self.input_encoder(x)
+        x_enc = x.clone()
+        x = self.encoder(x)
+        return x, x_enc
+
+
+class GroupConvolution(nn.Module):
+    def __init__(self, input_channels, output_channels, kernel_size, stride, padding, groups, act_norm=False):
+        super(GroupConvolution, self).__init__()
+        self.k = kernel_size
+        self.s = stride
+        self.p = padding
+        self.a_normalization = act_norm
+        if input_channels % groups != 0:
+            groups = 1
+        self.convolution = nn.Conv2d(input_channels, output_channels, kernel_size=self.k, stride=self.s,
+                                     padding=self.p, groups=groups)
+        self.normalization = nn.GroupNorm(groups, output_channels)
+        self.activation = nn.LeakyReLU(0.2)
+
+    def forward(self, x):
+        y_label = self.convolution(x)
+        if self.a_normalization:
+            y_norm = self.normalization(y_label)
+            y_label = self.activation(y_norm)
+        return y_label
+
+
+class InceptionModule(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, inception_kernel=[3, 5, 7, 11], groups=8):
+        super().__init__()
+        self.k_size = 1
+        self.s_size = 1
+        list_layers = []
+        self.convolution = nn.Conv2d(input_dim, hidden_dim, kernel_size=self.k_size, stride=self.s_size, padding=0)
+        for k in inception_kernel:
+            list_layers.append(
+                GroupConvolution(hidden_dim, output_dim, kernel_size=k, stride=self.s_size, padding=k // 2,
+                                 groups=groups, act_norm=True))
+        self.layers = nn.Sequential(*list_layers)
+
+    def forward(self, x):
+        x = self.convolution(x)
+        y_label = 0
+        for layer in self.layers:
+            y_label += layer(x)
+        return y_label
+
+
+class InceptionBridge(nn.Module):
+    def __init__(self, input_channels, hidden_channels, N_T, inception_kernel=[3, 5, 7, 11], groups=8):
+        super().__init__()
+        self.N_T = N_T
+        # encoder.
+        encoder_layers = [
+            InceptionModule(input_channels, hidden_channels // 2, hidden_channels, inception_kernel=inception_kernel,
+                            groups=groups)]
+        for i in range(1, N_T - 1):
+            encoder_layers.append(InceptionModule(hidden_channels, hidden_channels // 2, hidden_channels,
+                                                  inception_kernel=inception_kernel, groups=groups))
+        encoder_layers.append(
+            InceptionModule(hidden_channels, hidden_channels // 2, hidden_channels, inception_kernel=inception_kernel,
+                            groups=groups))
+        # decoder.
+        decoder_layers = [
+            InceptionModule(hidden_channels, hidden_channels // 2, hidden_channels, inception_kernel=inception_kernel,
+                            groups=groups)]
+        for i in range(1, N_T - 1):
+            decoder_layers.append(InceptionModule(2 * hidden_channels, hidden_channels // 2, hidden_channels,
+                                                  inception_kernel=inception_kernel, groups=groups))
+        decoder_layers.append(InceptionModule(2 * hidden_channels, hidden_channels // 2, input_channels,
+                                              inception_kernel=inception_kernel, groups=groups))
+        # self vars.
+        self.encoder = nn.Sequential(*encoder_layers)
+        self.decoder = nn.Sequential(*decoder_layers)
+
+    def forward(self, x):
+        B, T, C, H, W = x.shape
+        x = x.reshape(B, T * C, H, W)
+
+        # Encoder.
+        list_pass = []
+        z_hid = x
+        for i in range(self.N_T):
+            z_hid = self.encoder[i](z_hid)
+            if (i < self.N_T - 1):
+                list_pass.append(z_hid)
+
+        # Decoder.
+        z_hid = self.decoder[0](z_hid)
+        for i in range(1, self.N_T):
+            z_hid = self.decoder[i](torch.cat([z_hid, list_pass[-i]], dim=1))
+
+        y_label = z_hid.reshape(B, T, C, H, W)
+        return y_label
+
+
+class CNN_Decoder(nn.Module):
+    def __init__(self, hidden_channels, output_channels):
+        super().__init__()
+        self.k_size = 3
+        self.p_size = 1
+        self.output_p = 1
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(hidden_channels, hidden_channels, kernel_size=self.k_size, stride=2, padding=self.p_size,
+                               output_padding=self.output_p),
+            nn.GroupNorm(2, hidden_channels),
+            nn.LeakyReLU(0.2),
+
+            nn.ConvTranspose2d(hidden_channels, hidden_channels, kernel_size=self.k_size, stride=1,
+                               padding=self.p_size),
+            nn.GroupNorm(2, hidden_channels),
+            nn.LeakyReLU(0.2),
+
+            nn.ConvTranspose2d(hidden_channels, hidden_channels, kernel_size=self.k_size, stride=2, padding=self.p_size,
+                               output_padding=self.output_p),
+            nn.GroupNorm(2, hidden_channels),
+            nn.LeakyReLU(0.2))
+        self.output_decoder = nn.Sequential(
+            nn.ConvTranspose2d(2 * hidden_channels, hidden_channels, kernel_size=self.k_size, stride=1,
+                               padding=self.p_size),
+            nn.GroupNorm(2, hidden_channels),
+            nn.LeakyReLU(0.2))
+
+        self.output = nn.Conv2d(hidden_channels, output_channels, 1)
+
+    def forward(self, x, encoding):
+        x = self.decoder(x)
+        y_label = self.output_decoder(torch.cat([x, encoding], dim=1))
+        y_label = self.output(y_label)
+        return y_label
+
+
+class DLModelVideoPrediction(nn.Module):
+    '''
+    example usage:
+    model = DLModelVideoPrediction((11, 3, 160, 240), 64, 512, groups=4)
+    model = nn.DataParallel(model)
+    model = model.to(device)
+    '''
+    def __init__(self, input_dim, hidden_size=16, translator_size=256, inception_kernel=[3, 5, 7, 11], groups=8):
+        super().__init__()
+        T, C, H, W = input_dim
+        self.encoding = CNN_Encoder(C, hidden_size)
+        self.hidden = InceptionBridge(T * hidden_size, translator_size, 8, inception_kernel, groups)
+        self.decoding = CNN_Decoder(hidden_size, C)
+
+    def forward(self, x_orig):
+        B, T, C, H, W = x_orig.shape
+        x = x_orig.view(B * T, C, H, W)
+
+        embed, pass_ = self.encoding(x)
+        _, C_, H_, W_ = embed.shape
+
+        z = embed.view(B, T, C_, H_, W_)
+        hidden_el = self.hidden(z)
+        hidden_el = hidden_el.reshape(B * T, C_, H_, W_)
+
+        Y = self.decoding(hidden_el, pass_)
+        Y = Y.reshape(B, T, C, H, W)
+        return Y
