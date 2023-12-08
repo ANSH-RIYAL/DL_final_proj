ecd282a26e482020870d57c116d52eaa62f51255
Shreemayi <shreemayisonti7@gmail.com>
2023-12-08 12:46:22 -0500

    Added hidden_eval for combined output

--- hidden_eval.py
+++ hidden_eval.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+import torchmetrics
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
@@ -20,12 +21,12 @@ torch.cuda.empty_cache()
 class CustomDataset(Dataset):
     def __init__(self, num_of_vids=1000, evaluation_mode=False):
         self.evaluation_mode = evaluation_mode
-        if self.evaluation_mode:
+        if self.evaluation_mode == 'hidden':
             self.mode = 'hidden'
             start_num = 15000
-        else:
-            self.mode = 'train'
-            start_num = 0
+        elif self.evaluation_mode == 'val':
+            self.mode = 'val'
+            start_num = 1000
         self.vid_indexes = torch.tensor([i for i in range(start_num, num_of_vids + start_num)])
         self.num_of_vids = num_of_vids
 
@@ -49,7 +50,7 @@ class CustomDataset(Dataset):
             x.append(torch.tensor(plt.imread(filepath + f'image_{j}.png')).permute(2, 0, 1))
         x = torch.stack(x, 0)
 
-        if self.evaluation_mode:
+        if self.evaluation_mode == 'hidden':
             return x
 
         file_path = f"{base_dir}{self.mode}/video_{i}/mask.npy"
@@ -84,19 +85,22 @@ class combined_model(nn.Module):
 
     def forward(self, x):
         x = self.frame_prediction_model(x)
-        #         print(x.shape)
         x = x[:, -1]
-        #         print(x.shape)
         x = self.image_segmentation_model(x)
-        #         print(x.shape)
         return x
 
 
 # Create Val DataLoader
 batch_size = 8
-num_val_videos = 2000
-val_data = CustomDataset(num_val_videos, evaluation_mode=True)
+num_val_videos = 1000
+val_data = CustomDataset(num_val_videos, evaluation_mode='val')
 val_loader = DataLoader(val_data, batch_size=batch_size)
+
+hidden = False
+# num_hidden_videos = 2000
+# hidden_data = CustomDataset(num_hidden_videos, evaluation_mode='hidden')
+# hidden_loader = DataLoader(hidden_data, batch_size=batch_size)
+# hidden_pbar = tqdm(hidden_loader)
 
 model = combined_model(device)
 model = nn.DataParallel(model)
@@ -108,13 +112,29 @@ model.eval()
 val_pbar = tqdm(val_loader)
 
 with torch.no_grad():
-    preds = []
-    for batch_x in val_pbar:
-        batch_x, batch_y = batch_x.to(device), batch_y.to(device)
-        out = model(batch_x)
-        batch_out = np.argmax(out.detach().cpu().numpy(), axis=1)
-        preds.append(batch_out)
-    preds = np.concatenate(preds, axis=0)
+    if not hidden:
+        preds = []
+        total_y = []
+        for batch_x, batch_y in val_pbar:
+            batch_x = batch_x.to(device)
+            out = model(batch_x)
+            batch_out = torch.argmax(out.detach().cpu(), dim=1)
+            preds.append(batch_out)
+            total_y.append(batch_y)
+        preds = torch.cat(preds, dim=0)
+        ground_truth = torch.cat(total_y, dim=0)
+        jaccard = torchmetrics.JaccardIndex(task="multiclass", num_classes=49)
+        final_iou = jaccard(preds, ground_truth)
+        print(preds.shape, ground_truth.shape)
+        print("Final iou on val", final_iou)
+    else:
+        preds = []
+        for batch_x in hidden_pbar:
+            batch_x = batch_x.to(device)
+            out = model(batch_x)
+            batch_out = torch.argmax(out.detach().cpu(), dim=1)
+            preds.append(batch_out)
 
-print(preds.shape)
-torch.save(preds, 'leaderboard_2_team_13.pt')
+        preds = torch.cat(preds, dim=0)
+        print(preds.shape)
+        torch.save(preds, 'leaderboard_2_team_13.pt')
